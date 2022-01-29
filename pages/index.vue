a0e5e0bf75a9b724564b847982ba8992b5269974
Rasel Ahmed <wp@perfectpointmarketing.com>
2022-01-29 20:11:06 +0300

    Homepage Done

--- pages/index.vue
+++ pages/index.vue
@@ -123,6 +123,203 @@
             </svg>
           </template>
         </vue-slick-carousel>
+
+        <div class="flex -mx-6 mb-14">
+          <div class="w-1/2 px-6">
+            <div class="promo-bg-1 bg-cover bg-gray-500 bg-center p-12 rounded">
+              <div class="w-2/3">
+                <p class="bs-dark-orange-color text-2xl mb-6">Buy 1 Get 1</p>
+                <h3 class="font-bold font-size-46 mb-8 leading-tight">Fresh Fruits Collection</h3>
+                <nuxt-link to="/" class="bs-white-btn">Order Now</nuxt-link>
+              </div>
+            </div>
+          </div>
+
+          <div class="w-1/2 px-6">
+            <div class="promo-bg-2 bg-cover bg-gray-500 bg-center p-12 rounded">
+              <div class="w-2/3">
+                <p class="bs-dark-orange-color text-2xl mb-6">Buy 1 Get 1</p>
+                <h3 class="font-bold font-size-46 mb-8 leading-tight">Fresh Fruits Collection</h3>
+                <nuxt-link to="/" class="bs-white-btn">Order Now</nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <h3 class="text-4xl mb-6">Deals of the Week</h3>
+        <div class="flex items-center mb-10">
+          <div style="height: 3px" class="w-32 bs-dark-green-bg"></div>
+          <div style="height: 2px" class="w-full bg-gray-200"></div>
+        </div>
+
+        <vue-slick-carousel class="category-carousel mb-16 text-center" v-bind="productCarouselSettings">
+          <div class="single-bs-product">
+            <div class="h-80 relative mb-6">
+              <div class="h-full bg-gray-50 flex justify-center items-center p-4">
+                <img src="~/assets/img/fresh-fruit.png" alt="">
+              </div>
+              <div class="product-img-hover absolute h-full w-full top-0 left-0 flex justify-center items-center">
+                <div class="bg-black absolute h-full w-full opacity-60"></div>
+
+                <nuxt-link class="absolute left-0 bottom-0 bg-gray-200 p-2 w-full flex items-center justify-center" to="/">Details <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                </svg></nuxt-link>
+
+
+                <div class="relative z-10">
+                  <div class="flex justify-center items-center text-4xl text-white mb-8">
+                    <button class="h-12 w-12 border border-white rounded-full">-</button>
+                    <span class="mx-6">0</span>
+                    <button class="h-12 w-12 border border-white rounded-full">+</button>
+                  </div>
+
+                  <button class="bs-dark-green-bg text-white px-8 py-2 rounded-full inline-block">Add to card</button>
+                </div>
+              </div>
+            </div>
+
+            <h4 class="text-xl mb-3">Product Name</h4>
+            <p><span class="font-medium bs-dark-orange-color">$200.00</span> <del class="text-gray-400">$300</del></p>
+          </div>
+
+          <div class="">
+            <div class="bg-gray-50 h-80 flex justify-center items-center p-4 mb-6">
+              <img src="~/assets/img/grocery-items.png" alt="">
+            </div>
+
+            <h4 class="text-xl mb-3">Product Name</h4>
+            <p><span class="font-medium bs-dark-orange-color">$200.00</span> <del class="text-gray-400">$300</del></p>
+          </div>
+
+          <div class="">
+            <div class="bg-gray-50 h-80 flex justify-center items-center p-4 mb-6">
+              <img src="~/assets/img/vegetable-collection.png" alt="">
+            </div>
+
+            <h4 class="text-xl mb-3">Product Name</h4>
+            <p><span class="font-medium bs-dark-orange-color">$200.00</span> <del class="text-gray-400">$300</del></p>
+          </div>
+
+          <div class="">
+            <div class="bg-gray-50 h-80 flex justify-center items-center p-4 mb-6">
+              <img src="~/assets/img/fresh-fruit.png" alt="">
+            </div>
+
+            <h4 class="text-xl mb-3">Product Name</h4>
+            <p><span class="font-medium bs-dark-orange-color">$200.00</span> <del class="text-gray-400">$300</del></p>
+          </div>
+
+          <div class="">
+            <div class="bg-gray-50 h-80 flex justify-center items-center p-4 mb-6">
+              <img src="~/assets/img/grocery-items.png" alt="">
+            </div>
+
+            <h4 class="text-xl mb-3">Product Name</h4>
+            <p><span class="font-medium bs-dark-orange-color">$200.00</span> <del class="text-gray-400">$300</del></p>
+          </div>
+
+          <template #prevArrow="arrowOption">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+          </template>
+
+          <template #nextArrow="arrowOption">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </template>
+        </vue-slick-carousel>
+
+
+        <h3 class="text-4xl mb-6">Popular Brands</h3>
+        <div class="flex items-center mb-10">
+          <div style="height: 3px" class="w-32 bs-dark-green-bg"></div>
+          <div style="height: 2px" class="w-full bg-gray-200"></div>
+        </div>
+
+        <vue-slick-carousel class="category-carousel mb-16 text-center" v-bind="categoryCarouselSettings">
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-1.png" alt=""></div>
+          </div>
+
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-2.png" alt=""></div>
+          </div>
+
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-3.png" alt=""></div>
+          </div>
+
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-4.png" alt=""></div>
+          </div>
+
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-5.png" alt=""></div>
+          </div>
+
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-6.png" alt=""></div>
+          </div>
+
+          <div class="">
+            <div class="h-32 flex justify-center items-center"><img src="~/assets/img/brand-1.png" alt=""></div>
+          </div>
+
+
+
+          <template #prevArrow="arrowOption">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+          </template>
+
+          <template #nextArrow="arrowOption">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+            </svg>
+          </template>
+        </vue-slick-carousel>
+
+        <div class="flex -mx-6 mb-12">
+          <div class="w-1/3 px-6">
+            <div class="cta-1 flex p-6 rounded-xl">
+              <div class="w/1-2">
+                <h3 class="font-size-32 leading-tight font-semibold mb-10">Fresh Fruits Collection</h3>
+                <nuxt-link to="/" class="h-12 w-12 flex justify-center items-center rounded-full bg-white inline-block"><img src="~/assets/img/arrow-right.png" alt=""></nuxt-link>
+              </div>
+              <div class="w-1/2 flex justify-end">
+                <img src="~/assets/img/fresh-fruit.png" alt="">
+              </div>
+            </div>
+          </div>
+
+          <div class="w-1/3 px-6">
+            <div class="cta-2 flex p-6 rounded-xl">
+              <div class="w/1-2">
+                <h3 class="font-size-32 leading-tight font-semibold mb-10">Vegetable Collection</h3>
+                <nuxt-link to="/" class="h-12 w-12 flex justify-center items-center rounded-full bg-white inline-block"><img src="~/assets/img/arrow-right.png" alt=""></nuxt-link>
+              </div>
+              <div class="w-1/2 flex justify-end">
+                <img src="~/assets/img/vegetable-collection.png" alt="">
+              </div>
+            </div>
+          </div>
+
+          <div class="w-1/3 px-6">
+            <div class="cta-3 flex p-6 rounded-xl">
+              <div class="w/1-2">
+                <h3 class="font-size-32 leading-tight font-semibold mb-10">Grocery Items</h3>
+                <nuxt-link to="/" class="h-12 w-12 flex justify-center items-center rounded-full bg-white inline-block"><img src="~/assets/img/arrow-right.png" alt=""></nuxt-link>
+              </div>
+              <div class="w-1/2 flex justify-end">
+                <img class="h-40" src="~/assets/img/grocery-items.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </div>
@@ -138,8 +335,10 @@ export default {
   data() {
     return {
       categoryCarouselSettings: {
-        "slidesToShow": 6,
-        "prevArrow": '>'
+        "slidesToShow": 6
+      },
+      productCarouselSettings: {
+        "slidesToShow": 4
       }
     }
   }
